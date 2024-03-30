4dc70ca103f4386c437268c3b86fc57de7712a78
devhmac <devhmac@gmail.com>
2024-03-30 17:26:42 -0600

    taglining

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <main className=" flex min-h-screen flex-col items-center p-24">
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"> */}
-      <div className="mb-4 text-center">
-        <h1 className="mt-4 text-4xl font-semibold tracking-[-1.2px] sm:text-6xl sm:leading-[72px]">
+      <div className="mb-6 text-center">
+        <h1 className="mb-4 mt-4 text-5xl font-semibold tracking-[-1.2px] sm:text-7xl sm:leading-[72px]">
           Focus in parallel with{" "}
           <span className=" bg-gradient-to-tr from-sky-400 to-purple-500 bg-clip-text text-transparent dark:from-sky-300 dark:to-purple-300">
             Pomsly
@@ -23,8 +23,9 @@ export default function Home() {
             className="mx-2 mb-2 inline"
           /> */}
         </h1>
-        <p className="text-slate-400">
-          A Live Co-Working Platform to do something with others blab blab blah
+        <p className="text-md text-slate-300 sm:text-xl">
+          A live Co-working Platform to enhance your productivity,
+          colaboratively or independently.
         </p>
       </div>
       <TodoTable columns={columns} data={data} />
