119deb1a7e0c9541e9eb280ef5079d2a134073c2
devhmac <devhmac@gmail.com>
2024-03-30 16:02:30 -0600

    more logo and theming

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -6,9 +6,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className=" flex min-h-screen flex-col items-center p-24">
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"> */}
-      {/* <TodoTable columns={columns} data={data} /> */}
+      <TodoTable columns={columns} data={data} />
+      <div className="mb-4 text-center">
+        <h1 className="mt-4 text-4xl font-semibold tracking-[-1.2px] sm:text-6xl sm:leading-[72px]">
+          Focus in parallel with Pomsly
+          {/* Co-Work with others */}
+        </h1>
+        <p className="text-slate-400">
+          A Live Co-Working Platform to do something with others blab blab blah
+        </p>
+      </div>
       <Timer />
       {/* </div> */}
     </main>
