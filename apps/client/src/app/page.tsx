f39967b43be6b5978584b03ea6537200eea022dc
devhmac <devhmac@gmail.com>
2024-03-30 14:26:20 -0600

    adding task table

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Timer } from "@/components/Timer";
+import { Payment, columns } from "@/components/table/columns";
+import { data } from "@/components/table/sample_data";
+import { TodoTable } from "@/components/table/TodoTable";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"> */}
+      <TodoTable columns={columns} data={data} />
       <Timer />
       {/* </div> */}
     </main>
