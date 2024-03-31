ea307b5f468426d8c1c869c2103af9a34f77077f
Devin MacGillivray <devhmac@gmail.com>
2024-03-31 09:06:09 -0600

    messing with layout

--- apps/client/src/components/DashboardLayout.tsx
+++ apps/client/src/components/DashboardLayout.tsx
@@ -7,13 +7,17 @@ import { data } from "@/components/table/sample_data";
 
 const DashboardLayout = () => {
   return (
-    <div className="flex h-screen flex-row flex-wrap-reverse items-center justify-between">
-      <div className="min-w-[300px]">
-        <TodoTable columns={columns} data={data} />
+    <>
+      <div className="pt-[61px] text-center">
+        <Timer />
+        <div className="flex h-screen flex-row flex-wrap-reverse items-center justify-between">
+          <div className="min-w-[300px]">
+            <TodoTable columns={columns} data={data} />
+          </div>
+          <Chat />
+        </div>
       </div>
-      <Timer />
-      <Chat />
-    </div>
+    </>
   );
 };
 
--- apps/client/src/components/LandingLayout.tsx
+++ apps/client/src/components/LandingLayout.tsx
@@ -8,22 +8,16 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 
 const LandingLayout = () => {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center ">
       <MaxWidthWrapper className="sm:mt-30 mb-12 mt-28 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200/50 bg-popover/25 px-7 py-2 shadow-md backdrop-blur-sm transition-all hover:bg-accent/50">
           <p className="text-sm font-semibold text-primary">v0.7 Now Live!</p>
         </div>
         <Header />
       </MaxWidthWrapper>
-
       <Timer />
 
       <div>
-        <div
-          aria-hidden="true"
-          className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        ></div>
-
         <div>
           <div className="mx-auto max-w-6xl px-6 lg:px-8">
             <div className="mt-16 flow-root sm:mt-24">
@@ -44,7 +38,7 @@ const LandingLayout = () => {
       <Button className="hover:accent/50 bg-gradient" variant="outline">
         Start Live Space
       </Button>
-    </>
+    </div>
   );
 };
 
