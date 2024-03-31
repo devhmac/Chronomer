bbda6fd3a425b03e15715bba54d0848a823b10ef
devhmac <devhmac@gmail.com>
2024-03-30 20:59:43 -0600

    added timeline component to check out in readme

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
       <h1 className="sm:leading-[72px mb-4 mt-4 max-w-3xl text-5xl font-semibold tracking-[-1.2px] sm:text-7xl">
         {" "}
         <span className=" bg-gradient-to-tr from-sky-400 to-purple-500 bg-clip-text text-transparent dark:from-sky-300 dark:to-purple-300">
-          Pomsly
+          Cronomer
         </span>{" "}
         - Focus with others in parallel
         {/* Co-Work with others */}
