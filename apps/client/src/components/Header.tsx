2e3b71b66a282a9127be2fae5e201aca01a2bb03
devhmac <devhmac@gmail.com>
2024-04-01 07:31:11 -0600

    header text layout centering

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -2,20 +2,21 @@ import React from "react";
 
 const Header = () => {
   return (
-    <section className="mb-6 text-center">
-      <h1 className="sm:leading-[72px mb-4 mt-4 max-w-3xl text-5xl font-semibold tracking-[-1.2px] sm:text-7xl">
-        {" "}
+    <>
+      {/* <section className="mb-6 flex flex-col justify-center text-center"> */}
+      <h1 className="mb-4 mt-4 max-w-[850px] text-4xl font-semibold tracking-[-1.2px] sm:text-7xl sm:leading-[72px]">
         <span className=" bg-gradient-to-tr from-sky-400 to-purple-500 bg-clip-text text-transparent dark:from-sky-300 dark:to-purple-300">
-          Cronomer
+          Chronomer
         </span>{" "}
-        - Deep work with others in parallel
+        - Deep work in parallel with others
         {/* Co-Work with others */}
       </h1>
       <p className="text-md text-slate-300 sm:text-xl">
         A live Co-working platform to enhance your productivity, colaboratively
         or independently.
       </p>
-    </section>
+      {/* </section> */}
+    </>
   );
 };
 
--- apps/client/src/components/LandingLayout.tsx
+++ apps/client/src/components/LandingLayout.tsx
@@ -10,7 +10,7 @@ const LandingLayout = () => {
   return (
     <div className="flex flex-col items-center justify-center ">
       <MaxWidthWrapper className="sm:mt-30 mb-12 mt-28 flex flex-col items-center justify-center text-center">
-        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200/50 bg-popover/25 px-7 py-2 shadow-md backdrop-blur-sm transition-all hover:bg-accent/50">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200/50 bg-popover/25 px-6 py-2 shadow-md backdrop-blur-sm transition-all hover:bg-accent/50">
           <p className="text-sm font-semibold text-primary">v0.7 Now Live!</p>
         </div>
         <Header />
@@ -19,7 +19,7 @@ const LandingLayout = () => {
 
       <div>
         <div>
-          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className="max-w-8xl mx-auto px-6 lg:px-8">
             <div className="mt-16 flow-root sm:mt-24">
               <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                 <TodoTable columns={columns} data={data} />
