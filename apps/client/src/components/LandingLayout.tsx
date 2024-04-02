61314b11dc2cf45ebb64e7b6a62ed26ef4c01e4c
Devin MacGillivray <52307383+devhmac@users.noreply.github.com>
2024-04-02 09:03:01 -0600

    Update LandingLayout.tsx

--- apps/client/src/components/LandingLayout.tsx
+++ apps/client/src/components/LandingLayout.tsx
@@ -12,7 +12,7 @@ const LandingLayout = () => {
       <MaxWidthWrapper className="sm:mt-30 mb-12 mt-28 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200/50 bg-popover/25 px-6 py-2 shadow-md backdrop-blur-sm transition-all hover:bg-accent/50">
 
-          <p className="text-sm font-semibold text-primary">Coming Soon!</p>
+          <p className="text-sm font-semibold text-primary">v0.5 - More Coming Soon!</p>
 
         </div>
         <Header />
