8c6bcd43d3110bbcf134368ff9246b645b17d406
Devin MacGillivray <devhmac@gmail.com>
2024-04-21 17:25:34 -0600

    attempting to share types accross repo via package, did not work

---
+++ packages/shared-types/index.ts
@@ -0,0 +1,17 @@
+export const myFunc = () => {
+  console.log("Hello!");
+};
+
+export type Task = {
+  id: string;
+  task: string;
+  descripton: string;
+  status: "COMPLETE" | "INPROGRESS" | "BACKLOG" | "BLOCKED";
+  isActive: boolean;
+  subTasks: Task[];
+  timeToComplete: number;
+  order: number;
+  createdAt: Date;
+  completedAt: Date | null;
+  timersComplete: number;
+};
