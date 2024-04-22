b1e5bcd97f48f0711d45d4a4e17a60692d7fc233
Devin MacGillivray <devhmac@gmail.com>
2024-04-22 07:19:39 -0600

    styling complete task

--- apps/client/src/components/table/NewTask.tsx
+++ apps/client/src/components/table/NewTask.tsx
@@ -9,7 +9,7 @@ const NewTask = () => {
   const { addTask } = useContext(taskContext);
 
   return (
-    <div className="h-full w-full rounded-sm border border-dashed border-input p-2 text-muted-foreground hover:cursor-pointer hover:text-primary ">
+    <div className="h-full w-full rounded-sm border border-dashed border-input p-2 text-muted-foreground hover:cursor-pointer hover:text-zinc-200 ">
       <p
         onClick={() => {
           const createdTask = new Task();
--- apps/client/src/components/table/cells/CompleteTask.tsx
+++ apps/client/src/components/table/cells/CompleteTask.tsx
@@ -1,10 +1,17 @@
+import { cn } from "@/lib/utils/utils";
 import { CircleCheckBig } from "lucide-react";
 import React from "react";
 
-const CompleteTask = () => {
+const CompleteTask = ({ task }) => {
+  const isComplete = false;
   return (
-    <div>
-      <CircleCheckBig className="h-4 w-4" />
+    <div className=" hover:cursor-pointer">
+      <CircleCheckBig
+        className={cn(
+          "h-4 w-4 text-zinc-400 ",
+          isComplete ? "text-green-400" : "text-zinc-400 hover:text-zinc-200",
+        )}
+      />
     </div>
   );
 };
