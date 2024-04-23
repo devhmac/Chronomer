84a0c229091890f30b49df921af696b38ae04de2
Devin MacGillivray <devhmac@gmail.com>
2024-04-23 07:45:36 -0600

    more type errors

--- apps/client/src/components/DashboardLayout.tsx
+++ apps/client/src/components/DashboardLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { TodoTable } from "./table/DataTable";
 import { Timer } from "./Timer";
 import Chat from "./chat/Chat";
-import { Payment, columns } from "@/components/table/columns";
+import { columns } from "@/components/table/columns";
 import { data } from "@/components/table/sample_data";
 
 const DashboardLayout = () => {
@@ -12,7 +12,7 @@ const DashboardLayout = () => {
         <Timer />
         <div className="flex h-screen flex-row flex-wrap-reverse items-center justify-between">
           <div className="min-w-[300px]">
-            <TodoTable columns={columns} data={data} />
+            {/* <TodoTable columns={columns} data={data} /> */}
           </div>
           <Chat />
         </div>
