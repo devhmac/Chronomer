b9099afc27c812619454a4573ca0db8c4372d650
devhmac <devhmac@gmail.com>
2024-04-26 07:36:35 -0600

    testing out vercel analytics

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { TimerContextProvider } from "@/context/TimerContext";
 import Nav from "@/components/ui/Nav";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +25,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         {/* <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div> */}
-
+        <Analytics />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
