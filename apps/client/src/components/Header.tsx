dfe05e6eb07dfdeaa2d90e698497a127802ed7b6
Devin MacGillivray <devhmac@gmail.com>
2024-05-02 21:51:19 -0600

    added a little more margin on landing header

--- apps/client/src/components/Header.tsx
+++ apps/client/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <>
       {/* <section className="mb-6 flex flex-col justify-center text-center"> */}
-      <h1 className="mb-4 mt-4 max-w-[850px] text-4xl font-semibold tracking-[-1.2px] sm:text-7xl sm:leading-[72px]">
+      <h1 className=" mb-9 mt-4  max-w-[850px] text-4xl font-semibold tracking-[-1.2px] sm:text-7xl sm:leading-[72px]">
         <span className=" bg-gradient-to-tr from-sky-400 to-purple-500 bg-clip-text text-transparent dark:from-sky-300 dark:to-purple-300">
           Chronomer
         </span>{" "}
