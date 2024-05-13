ed1c860c5bcff9f92c6d1e55fcce23d79e53691e
Devin MacGillivray <devhmac@gmail.com>
2024-05-13 16:37:52 -0600

    cleaning up page and testing a toggle state landing page

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,10 +1,4 @@
-import { Timer } from "@/components/Timer";
-
-import Image from "next/image";
-import Header from "@/components/Header";
-import { Button } from "@/components/ui/button";
 import LandingLayout from "@/components/LandingLayout";
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
