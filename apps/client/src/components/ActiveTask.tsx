2b9060da70576981de98253ba309b9817a1a6ba1
Devin MacGillivray <devhmac@gmail.com>
2024-05-15 07:09:19 -0600

    adding re-usable glass class, and active task comp almost working

--- apps/client/src/components/ActiveTask.tsx
+++ apps/client/src/components/ActiveTask.tsx
@@ -17,44 +17,50 @@ const ActiveTask = () => {
   return !activeTask ? (
     <div>no active task selected</div>
   ) : (
-    <Table className="max-w-full">
-      <TableBody>
-        <TableRow
-          key={activeTask.id}
-          className={
-            activeTask.isComplete
-              ? "bg-background-muted text-str text-zinc-500 line-through	"
-              : ""
-          }
-        >
-          <TableCell className="hidden sm:table-cell">
-            <div className="">
-              {activeTask.id === "-1" ? (
-                <CancelTask task={activeTask} />
-              ) : (
-                <CompleteTask task={activeTask} />
-              )}
-            </div>
-          </TableCell>
-          <TableCell className="min-w-[315px] @[560px]:bg-red-400  md:max-w-[315px]">
-            <TaskCell task={activeTask} />
-          </TableCell>
-          {/* <TableCell>
-                <Badge variant="outline">Draft</Badge>
-              </TableCell> */}
-          <TableCell className="hidden md:table-cell">
-            <StatusSelect task={activeTask} />
-          </TableCell>
-          <TableCell className="hidden md:table-cell">
-            {activeTask.timersComplete}/{activeTask.timeToComplete}
-          </TableCell>
+    <div className="glass-bg mb-2 flex flex-row items-center gap-4 rounded-md p-3">
+      <CompleteTask task={activeTask} />
+      <p>{activeTask.task}</p>
+      {/* <p>{activeTask.status}</p> */}
+      {/* <StatusSelect task={activeTask} /> */}
+    </div>
+    // <Table className="glass-bg mb-2 max-w-full rounded-sm">
+    //   <TableBody>
+    //     <TableRow
+    //       key={activeTask.id}
+    //       className={
+    //         activeTask.isComplete
+    //           ? "bg-background-muted text-str rounded-lg border-red-400 text-zinc-500 line-through	"
+    //           : ""
+    //       }
+    //     >
+    //       <TableCell className="hidden sm:table-cell">
+    //         <div className="">
+    //           {activeTask.id === "-1" ? (
+    //             <CancelTask task={activeTask} />
+    //           ) : (
+    //             <CompleteTask task={activeTask} />
+    //           )}
+    //         </div>
+    //       </TableCell>
+    //       <TableCell className="min-w-[315px] @[560px]:bg-red-400  md:max-w-[315px]">
+    //         <TaskCell task={activeTask} />
+    //       </TableCell>
+    //       {/* <TableCell>
+    //             <Badge variant="outline">Draft</Badge>
+    //           </TableCell> */}
+    //       <TableCell className="hidden md:table-cell">
+    //         <StatusSelect task={activeTask} />
+    //       </TableCell>
+    //       <TableCell className="hidden md:table-cell">
+    //         {activeTask.timersComplete}/{activeTask.timeToComplete}
+    //       </TableCell>
 
-          <TableCell>
-            <Options task={activeTask} />
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    //       <TableCell>
+    //         <Options task={activeTask} />
+    //       </TableCell>
+    //     </TableRow>
+    //   </TableBody>
+    // </Table>
   );
 };
 
