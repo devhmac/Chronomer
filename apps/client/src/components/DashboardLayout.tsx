7eae53b0bddaf68672e7e38da36429a6179ee224
Devin MacGillivray <devhmac@gmail.com>
2024-05-19 19:54:27 -0600

    enabling scroll when not in wide mode

--- apps/client/src/components/DashboardLayout.tsx
+++ apps/client/src/components/DashboardLayout.tsx
@@ -8,28 +8,26 @@ import ActiveTask from "./ActiveTask";
 
 const DashboardLayout = () => {
   return (
-    <>
-      <div className="h-screen ">
-        {/* flexbox version */}
-        <div className="relative flex  h-full flex-col-reverse flex-wrap-reverse items-center justify-center gap-2 xl:flex-row  ">
-          {/* grid version */}
-          {/* <div className="grid grid-cols-3 gap-2 pt-[61px] text-center"> */}
-          {/* <div className=""> */}
-          {/* <div className="min-w-[300px]"> */}
-          {/* <TodoTable columns={columns} data={data} /> */}
-          <div className="w-full sm:w-auto xl:h-full xl:pt-[61px]">
-            <ToDoWrapper className="xl:border-l-0" />
-          </div>
-          <div className="mx-auto w-full pt-[61px] sm:w-fit xl:pt-0">
-            <ActiveTask />
-            <Timer />
-          </div>
-          {/* <div className="- h-full w-[600px] border border-red-200">
+    <div className="xl:h-screen">
+      {/* flexbox version */}
+      <div className="relative flex h-full flex-col-reverse flex-wrap-reverse items-center justify-center gap-2 xl:flex-row  ">
+        {/* grid version */}
+        {/* <div className="grid grid-cols-3 gap-2 pt-[61px] text-center"> */}
+        {/* <div className=""> */}
+        {/* <div className="min-w-[300px]"> */}
+        {/* <TodoTable columns={columns} data={data} /> */}
+        <div className="w-full sm:w-auto xl:h-full xl:pt-[61px]">
+          <ToDoWrapper className="xl:border-l-0" />
+        </div>
+        <div className="mx-auto w-full pt-[61px] sm:w-fit xl:pt-0">
+          <ActiveTask />
+          <Timer />
+        </div>
+        {/* <div className="- h-full w-[600px] border border-red-200">
             <p>this is chat</p>
           </div> */}
-        </div>
       </div>
-    </>
+    </div>
   );
 };
 
