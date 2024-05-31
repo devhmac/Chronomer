667e1a30f1128a3469f3c0bdece9fb85e9a18369
Devin MacGillivray <devhmac@gmail.com>
2024-05-31 07:54:45 -0600

    adding background progress bar to task time

--- apps/client/src/components/table/cells/TimeSpent.tsx
+++ apps/client/src/components/table/cells/TimeSpent.tsx
@@ -1,23 +1,47 @@
-import React from "react";
+import { cn } from "@/lib/utils/utils";
+import React, { useState } from "react";
 
 type props = {
-  timersComplete: number;
+  timeSpent: number;
   timeToComplete: number;
   taskId: string;
 };
 
-const TimeSpent = ({ timersComplete, timeToComplete, taskId }: props) => {
+const TimeSpent = ({ timeSpent, timeToComplete, taskId }: props) => {
   function padTwoDigits(num: number) {
     return num.toString().padStart(2, "0");
   }
+
+  const derivePercentComplete = (timeSpent: number, timeTarget: number) => {
+    if (timeSpent === 0 || timeToComplete === 0) return 0;
+    const decimalPercent = Number((timeSpent / timeTarget).toFixed(1));
+    return decimalPercent * 100;
+  };
+  const percentComplete = derivePercentComplete(timeSpent, timeToComplete);
+  // const percentComplete = 10;
+
   const minutesToTime = (time: number) => {
     const hours = Math.floor(time / 60);
     const min = time % 60;
-    return `${padTwoDigits(hours)}:${padTwoDigits(min)}`;
+    return hours === 0 ? `${min}m` : `${hours}h ${min}m`;
+
+    // return `${padTwoDigits(hours)}:${padTwoDigits(min)}`;
   };
 
-  const time = minutesToTime(timeToComplete);
-  return <div className="rounded-md border p-1 hover:bg-accent">{time}</div>;
+  const time = minutesToTime(timeSpent);
+  return (
+    <div className="relative overflow-hidden rounded-md border p-1 hover:bg-accent">
+      <div
+        className={cn(
+          " absolute inset-0  z-0 min-w-2 bg-green-800",
+          percentComplete >= 10 ? `w-[${percentComplete}%]` : "hidden",
+        )}
+      ></div>
+      <p className="relative z-10">
+        {minutesToTime(timeSpent)} / {minutesToTime(timeToComplete)}{" "}
+      </p>
+    </div>
+  );
 };
 
 export default TimeSpent;
