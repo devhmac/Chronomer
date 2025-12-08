718430e3126ad22baef7a755739780039f98ecb0
Devin MacGillivray <devhmac@gmail.com>
2025-12-07 17:45:03 -0700

    simple plate editor

--- apps/client/src/app/notes/page.tsx
+++ apps/client/src/app/notes/page.tsx
@@ -4,20 +4,14 @@ import { Plate, usePlateEditor } from "platejs/react";
 
 import { Editor, EditorContainer } from "@/components/ui/editor";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { PlateEditor } from "@/components/editor/plate-editor";
 
 export default function NotePage() {
-	const editor = usePlateEditor(); // Initializes the editor instance
+	// const editor = usePlateEditor(); // Initializes the editor instance
 	return (
 		<MaxWidthWrapper className="h-screen">
 			<div className="border border-black h-screen">
-				Notes Screen
-				<Plate editor={editor}>
-					{/* Provides editor context */}
-					<EditorContainer className="h-full border border-black">
-						{/* Styles the editor area */}
-						<Editor placeholder="Type your amazing content here..." />
-					</EditorContainer>
-				</Plate>{" "}
+				<PlateEditor />
 			</div>
 		</MaxWidthWrapper>
 	);
